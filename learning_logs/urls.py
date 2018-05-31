43a082018a43c4c9f3b294e53ede7660285c61ed
mashilu <mslbuaa@163.com>
2018-05-31 18:09:17 +0800

    ******

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     url(r'^topics/$', views.topics, name='topics'),
     url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
     url(r'^new_topic/$', views.new_topic, name='new_topic'),
+
+    # 添加新条目的页面
+    url(r'^add_entry/(?P<topic_id>\d+)/$', views.add_entry, name='add_entry'),
+
+    # 编辑条目entry的页面
+    url(r'^edit_entry/(?P<entry_id>\d+)/$', views.edit_entry, name='edit_entry'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
-from .form import TopicForm
+from .models import Entry
+from .forms import TopicForm
+from .forms import EntryForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -40,3 +42,41 @@ def new_topic(request):
             return HttpResponseRedirect(reverse('learning_logs:topics'))
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
+
+
+def add_entry(request, topic_id):
+    """在特定的主题中添加新条目"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # 未提交数据，创建一个空表单
+        form = EntryForm()
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/add_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """编辑既有条目entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # 初次请求，使用当前条目填充表单
+        form = EntryForm(instance=entry)
+    else:
+        # POST提交的数据，对数据进行处理
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
